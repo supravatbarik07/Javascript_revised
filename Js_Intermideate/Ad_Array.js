973d97388e1615326f1e846b2b0849fcf7af720f
Supravat Barik <supravatbarik8917@gmail.com>
2026-02-22 23:27:47 +0530

    first commit

--- Js_Intermideate/Ad_Array.js
+++ Js_Intermideate/Ad_Array.js
@@ -1,9 +1,9 @@
 const layer1=['BTC','ETH','BNB','LTC']
 const layer2=['POL','SOL','OP','ARB','LXP','MINT'];
 
-//layer1.push(layer2);// not a correct approch
+// layer1.push(layer2);// not a correct approch
 
-//console.log(layer1); //It hold array inside an array in the last index.
+// console.log(layer1); //It hold array inside an array in the last index.
 
 //concat --It can merge 2 array into a new array and don't hold any array 
 
@@ -17,6 +17,7 @@ const layer4=[...layer1,...layer2]
 
 //console.log(layer4);
 
+
 //Array Flat() method it is  combine all arrays inside an array
 
 const meme=['doge','dogs','pepe',['shiba','trump'],['goats',['baby_doge','lovely']]]
@@ -24,9 +25,9 @@ const meme=['doge','dogs','pepe',['shiba','trump'],['goats',['baby_doge','lovely
 const meme1=meme.flat(4) //Here argument pass as upto how many depth array includes, 
                         //If u pass infinity then it is also giving all values
 
-//console.log(meme1);
+// console.log(meme1);
 
-//console.log(...layer1,...meme); // it is combine only 1 depth 
+console.log(...layer1,...meme); // it is combine only 1 depth 
 
 //console.log(layer1.concat(meme)) //It can't concat the depth arrays
 
@@ -42,7 +43,7 @@ It will treat the object as an empty iterable, meaning that Array.from() won't b
 The resulting array will be an empty array ([]), because the object does not have numeric indices or a length property that Array.from() can use. */
 
 //correct way->
-//console.log(Object.keys({name:'pol'})) //[ 'name' ]
+console.log(Object.keys({name:'pol'})) //[ 'name' ]
 /** here we define the which element of object we want to create an array key or value */
 
 const coin1='Ada'
