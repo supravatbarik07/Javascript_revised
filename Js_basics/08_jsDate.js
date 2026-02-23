eb70e0d252f10b1975fdf6e5cce5dc5d1343883b
Supravat Barik <supravatbarik8917@gmail.com>
2026-02-23 12:58:25 +0530

    verification code added

--- Js_basics/08_jsDate.js
+++ Js_basics/08_jsDate.js
@@ -2,14 +2,20 @@
 
 const myDate=new Date();
 
-console.log(myDate.getDate());
-console.log(myDate.toDateString());
-// console.log(typeof myDate);
+// console.log(myDate.getDate());
+// console.log(myDate);
+// console.log(myDate.toJSON());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
 
-console.log(myDate.toISOString());
+
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);//date type is object in js
+
+// console.log(myDate.toISOString());
 const myDate1=new Date(2024,0,0) // in date  month numbers starts from 0
 
-//console.log(myDate1.toLocaleString());//Sat Jan 06 2024
+console.log(myDate1.toLocaleString());//Sat Jan 06 2024
 
 const myDate2=new Date(2024,0,6,5,45)
 //console.log(myDate2.toLocaleString());//1/6/2024, 5:45:00 AM
@@ -22,6 +28,9 @@ const myDate4= new Date('01-23-2024') // mm-dd-yyyy also available
 
 const myDate5=Date.now();//give current date and time in milliseconds
 
+let timeGap=myDate5-myDate4.getTime();
+// console.log(timeGap);
+
 //console.log(myDate5);
 // console.log(myDate6);
 // console.log(myDate4.getTime()); // give time and date in miliseconds 
@@ -29,11 +38,15 @@ const myDate5=Date.now();//give current date and time in milliseconds
 //console.log(Math.floor(Date.now()/1000)); // Give time and dt in seconds 
 
 const myDate6 = new Date();  // Create a Date object with the current date and time
-
+console.log(myDate6.getDate());
+console.log(myDate6.getDay());
+console.log(myDate6.getMonth()+1);// as it starts from 0 index so can add 1 for current month
+console.log(myDate6.getHours());
 
 
 const weekday = myDate6.toLocaleString('default', {
-    weekday: 'long'   // Get the full name of the day (e.g., "Monday")
+    //weekday: 'long',   // Get the full name of the day (e.g., "Monday")
+    dateStyle:'full'
 });
 
-console.log(weekday);  // This will log the name of the current day of the week
+ console.log(weekday);  // This will log the name of the current day of the week
--- login form/script.js
+++ login form/script.js
@@ -1,16 +1,41 @@
 
 let submit=document.getElementById('btn');
+let captcha_display=document.getElementById('captcha-code');
+let refresh=document.getElementById('refresh')
+let captcha_code='';
+function generateCaptcha(){
+    captcha_code='';
+    for(let i=1;i<5;i++){
+        let randomNo= Math.floor(Math.random()*10)
+        captcha_code+=randomNo;
+    }
+}
+generateCaptcha();
+// console.log(captcha_code);
+captcha_display.textContent=`${captcha_code}`
+
+refresh.addEventListener('click',()=>{
+    generateCaptcha();
+    captcha_display.textContent=`${captcha_code}`
+})
+
 
 
 submit.addEventListener('click',()=>{
     let emailInput = document.getElementById("email").value;
     let passwordInput=document.getElementById('password').value;
-    let genderSelect=document.querySelector('input[name="gender"]:checked')?.value
+    let captcha=document.getElementById('captcha').value;
+    // let genderSelect=document.querySelector('input[name="gender"]:checked')?.value
     document.querySelector('.error').innerHTML='';
+    if(captcha!==captcha_code){
+        document.querySelector('.error').innerHTML=`captcha doesn't match'`
+        return;
+    }
     if(emailInput.length===0 || passwordInput.length===0){
         document.querySelector('.error').innerHTML='Please filed both the filed'
         return;
     }
     document.querySelector('.alert-box').style.display="block"
     document.querySelector('.alert-box').textContent='Login sucessfully.'
+
 })
